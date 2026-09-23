0d49b3be1970ed11f6f4d93068bf7bfbc500d6c7
vuckoo0 <pedja.kg.1602@gmail.com>
2026-09-23 19:10:44 +0200

    Made a simple program for inputing a string and storing it in memory and then printing it

--- test.asm
+++ test.asm
@@ -2,8 +2,6 @@
 
 mov bx, buffer
 
-
-
 input_string:
 
     mov ah, 0
@@ -18,13 +16,47 @@ input_string:
     mov [bx], al
     inc bx
 
+    mov ah, 0x0e
+    int 0x10
     jmp input_string
 
 end:
 
-jmp $
+    mov ah, 0x0e
+
+    mov bx, newline
+    call print_string
+
+    mov bx, exit_statement
+    call print_string
+
+    mov bx, buffer
+    call print_string
+
+    mov bx, newline
+    call print_string
+
+    jmp loop_forever
+
+print_string:
+
+    mov al, [bx]
+
+    cmp al, 0
+    jne skip
+    ret
+    skip:
+
+    int 0x10
+    inc bx
+    jmp print_string
+
+loop_forever:
+    jmp $
 
 buffer: times 32 + 1 db 0
+exit_statement: db "Bye ", 0
+newline: db 0x0d, 0x0a, 0
 
 times 510 - ($ - $$) db 0
 db 0x55, 0xaa
